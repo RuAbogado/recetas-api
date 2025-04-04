a55f077569c49238fb5ef22d412e06cf5288b141
rubengo <20233tn207@utez.edu.mx>
2025-04-04 15:21:33 -0600

    api terminada

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// 👉 Servir carpeta de imágenes de forma pública
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Conexión a MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
